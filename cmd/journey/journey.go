f5059240e2fd3401a143022fff25515a5d430642
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-11 23:29:49 -0300

    feat: start server with chi

--- cmd/journey/journey.go
+++ cmd/journey/journey.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"journey/internal/api"
+	"journey/internal/api/spec"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/phenpessoa/gutils/netutils/httputils"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
 	ctx := context.Background()
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGKILL)
-    defer cancel()
+	defer cancel()
 
 	if err := run(ctx); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -21,8 +34,74 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-    select{
-    case <- ctx.Done():
-        return nil
-    }
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+
+	logger, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+
+	logger = logger.Named("journey_app")
+	defer func() { _ = logger.Sync() }()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("JOURNEY_DATABASE_USER"),
+		os.Getenv("JOURNEY_DATABASE_PASSWORD"),
+		os.Getenv("JOURNEY_DATABASE_HOST"),
+		os.Getenv("JOURNEY_DATABASE_PORT"),
+		os.Getenv("JOURNEY_DATABASE_NAME"),
+	),
+	)
+
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		return err
+	}
+
+	si := api.NewAPI(pool, logger)
+
+	r := chi.NewMux()
+	r.Use(middleware.RequestID, middleware.Recoverer, httputils.ChiLogger(logger))
+	r.Mount("/", spec.Handler(&si))
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	defer func() {
+		const timeout = 30 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); ctx != nil {
+			logger.Error("failed to shutdown server", zap.Error(err))
+		}
+	}()
+
+	errChan := make(chan error, 1)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			errChan <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-errChan:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+	}
+	return nil
 }
