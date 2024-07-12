dcd3b526916f403cca688e129cbcf69e7903b93b
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-11 21:46:12 -0300

    feat: use impl from Go lib

---
+++ internal/api/api.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"journey/internal/api/spec"
+	"net/http"
+)
+
+type API struct {}
+
+// Confirms a participant on a trip.
+// (PATCH /participants/{participantId}/confirm)
+func (ap *API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *http.Request, participantID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Create a new trip
+// (POST /trips)
+func (ap *API) PostTrips(w http.ResponseWriter, r *http.Request) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Get a trip details.
+// (GET /trips/{tripId})
+func (ap *API) GetTripsTripID(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Update a trip.
+// (PUT /trips/{tripId})
+func (ap *API) PutTripsTripID(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Get a trip activities.
+// (GET /trips/{tripId}/activities)
+func (ap *API) GetTripsTripIDActivities(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Create a trip activity.
+// (POST /trips/{tripId}/activities)
+func (ap *API) PostTripsTripIDActivities(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Confirm a trip and send e-mail invitations.
+// (GET /trips/{tripId}/confirm)
+func (ap *API) GetTripsTripIDConfirm(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Invite someone to the trip.
+// (POST /trips/{tripId}/invites)
+func (ap *API) PostTripsTripIDInvites(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Get a trip links.
+// (GET /trips/{tripId}/links)
+func (ap *API) GetTripsTripIDLinks(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Create a trip link.
+// (POST /trips/{tripId}/links)
+func (ap *API) PostTripsTripIDLinks(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
+
+// Get a trip participants.
+// (GET /trips/{tripId}/participants)
+func (ap *API) GetTripsTripIDParticipants(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+	panic("not implemented") // TODO: Implement
+}
