5f671ba641a98cd09afd208ac6b29b5e2d0ac18d
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-12 19:51:54 -0300

    feat: add validation in spec json and update gen to generate spec

--- gen.go
+++ gen.go
@@ -1,4 +1,5 @@
 package main
 
+//go:generate goapi-gen --package=spec --out ./internal/api/spec/journey.spec.go ./internal/api/spec/journey.spec.json run go generate ./... | run go generate
 //go:generate tern migrate --migrations ./internal/pgstore/migrations --config ./internal/pgstore/migrations/tern.conf run go generate ./... | run go generate
 //go:generate sqlc generate -f ./internal/pgstore/sqlc.yml
