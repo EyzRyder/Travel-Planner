09d0f5b76db5ae7d573ecb31c37319c78195b33f
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-11 22:15:02 -0300

    feat: add zap packege and implement patch

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,16 +1,65 @@
 package api
 
 import (
+	"context"
+	"errors"
 	"journey/internal/api/spec"
+	"journey/internal/pgstore"
 	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.uber.org/zap"
 )
 
-type API struct {}
+type store interface {
+	GetParticipant(ctx context.Context, id uuid.UUID) (pgstore.Participant, error)
+	ConfirmParticipant(ctx context.Context, id uuid.UUID) error
+}
+
+type API struct {
+	store  store
+	logger *zap.Logger // us.logger do stander lib tambem serve
+}
+
+func NewAPI(pool *pgxpool.Pool, logger *zap.Logger) API {
+    return API{pgstore.New(pool),logger}
+}
 
 // Confirms a participant on a trip.
 // (PATCH /participants/{participantId}/confirm)
 func (ap *API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *http.Request, participantID string) *spec.Response {
-	panic("not implemented") // TODO: Implement
+	id, err := uuid.Parse(participantID)
+	if err != nil {
+		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "uuid invalido"})
+	}
+
+	participant, err := ap.store.GetParticipant(r.Context(), id)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "participante não encontrado"})
+		}
+		ap.logger.Error("failed to get participant", zap.Error(err), zap.String("participant_id", participantID))
+		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{
+			Message: "something went wrong, try again",
+		})
+	}
+
+	if participant.IsConfirmed {
+		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{
+			Message: "participant already confirmed",
+		})
+	}
+	if err := ap.store.ConfirmParticipant(r.Context(), id); err != nil {
+		ap.logger.Error("failed to confim participant", zap.Error(err), zap.String("participant_id", participantID))
+		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{
+			Message: "something went wrong, try again",
+		})
+	}
+
+	return spec.PatchParticipantsParticipantIDConfirmJSON204Response(nil)
 }
 
 // Create a new trip
